51be4225a08cd085b72e3f54ecdb9e2be554d5d3
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2021-12-04 07:33:52 +0000

    Updated test_lambda to use the awscli
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- test_lambda.sh
+++ test_lambda.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-execute_function() {
+execute_lambda_function() {
+	echo "Calling lambda_function_$1"
+	echo "--------------------------"
 	json={"\"function_name\"":"\"lambda_function_$1\""}
-	time output=`curl -s -H "Content-Type: application/json" -X POST -d $json "https://urwkt891se.execute-api.us-east-2.amazonaws.com/prod"`
+	time output=$(aws lambda invoke --invocation-type RequestResponse --function-name tcss562_term_project --region us-east-2 --payload $json /dev/stdout | head -n 1 | head -c -2)
 	echo $output | jq
 }
 
-execute_function 1
-execute_function 2
-execute_function 3
+execute_lambda_function 1 && \
+execute_lambda_function 2 && \
+execute_lambda_function 3
