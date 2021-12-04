a2d1bf7642c0af063ac9948e8038eec36c389f19
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2021-12-04 14:39:04 +0000

    Modified scripts to work for other users
    test_lambda.sh was updated to have a long period before retrying.
    deploy scripts were made more or less user agnostic.
    install_awscli.sh now has a configuration step at the end.
    
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- deploy_lambda_ecr.sh
+++ deploy_lambda_ecr.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+cd `dirname $0`
+
+# Check if awscli is installed
+./install_awscli.sh
+
+# My container registry portal
+id=`aws sts get-caller-identity | jq -r '.["Account"]'`
+MY_ECR=$id.dkr.ecr.us-east-2.amazonaws.com
+unset id
+
 # This is the default project lambda function name
-MY_ECR=574778148684.dkr.ecr.us-east-2.amazonaws.com
 if [ "`uname -m`" == 'x86_64' ]; then
 	MY_FUNCTION='tcss562_term_project'
 else
 	MY_FUNCTION='tcss562_term_project_arm'
 fi
 
-cd `dirname $0`
-
-# Check if awscli is installed
-./install_awscli.sh
+# Create ECR repo (this step will fail if it exists but we will assume that all is well)
+aws ecr create-repository --repository-name $MY_FUNCTION 2> /dev/null
 
 # Build/deploy Docker image
 docker build -t $MY_ECR/$MY_FUNCTION .
--- install_awscli.sh
+++ install_awscli.sh
@@ -8,3 +8,5 @@ fi
 curl "https://awscli.amazonaws.com/awscli-exe-linux-`uname -m`.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+printf "\n\nEnter in credentials when prompted, generate in the AWS Console under 'Security credentials' -> 'Access keys'\n\n"
+aws configure
