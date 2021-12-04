72854cd7669ebb4ac6fba5ad2006a21e3b0e87ee
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2021-12-04 06:41:48 +0000

    Code cleanup
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- deploy_lambda_ecr.sh
+++ deploy_lambda_ecr.sh
@@ -2,24 +2,19 @@
 
 # This is the default project lambda function name
 MY_ECR=574778148684.dkr.ecr.us-east-2.amazonaws.com
+if [ "`uname -m`" == 'x86_64' ]; then
+	MY_FUNCTION='tcss562_term_project'
+else
+	MY_FUNCTION='tcss562_term_project_arm'
+fi
 
 cd `dirname $0`
 
 # Check if awscli is installed
 ./install_awscli.sh
 
-if [ "`uname -m`" == 'x86_64' ]; then
-	DOCKERFILE='Dockerfile_x86'
-	MY_FUNCTION='tcss562_term_project'
-else
-	DOCKERFILE='Dockerfile_arm'
-	MY_FUNCTION='tcss562_term_project_arm'
-fi
-
 # Build/deploy Docker image
-docker system prune -f
-docker build -f $DOCKERFILE -t $MY_FUNCTION .
-docker tag $MY_FUNCTION:latest $MY_ECR/$MY_FUNCTION:latest
+docker build -t $MY_ECR/$MY_FUNCTION .
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin $MY_ECR
 docker push $MY_ECR/$MY_FUNCTION:latest
 
--- test_lambda.sh
+++ test_lambda.sh
@@ -2,7 +2,7 @@
 
 execute_function() {
 	json={"\"function_name\"":"\"lambda_function_$1\""}
-	output=curl -s -H "Content-Type: application/json" -X POST -d $json "https://urwkt891se.execute-api.us-east-2.amazonaws.com/prod"
+	time output=`curl -s -H "Content-Type: application/json" -X POST -d $json "https://urwkt891se.execute-api.us-east-2.amazonaws.com/prod"`
 	echo $output | jq
 }
 
