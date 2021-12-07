7965653221b52ebb1411e85b06b5ff319110a7c9
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2021-12-07 23:55:51 +0000

    arm and x86 results will be stored in two different buckets for easier sorting and comparison
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import topic_model
 import json
 import logging
 import s3
+import platform
 
 def handler(event, context):
     # Collect data
@@ -16,12 +17,21 @@ def handler(event, context):
 
     inspector.inspectAllDeltas()
     results = inspector.finish()
+    # If newcontainer is 0 then we have a warm start, otherwise cold start
     if results['newcontainer'] == 0:
         state="warm"
     else:
         state="cold"
+
+    # Determine CPU arch for S3 buckets
+    if platform.machine() == 'x86_64':
+        arch = 'x86'
+    else:
+        arch = 'arm'
+
     # format a filename
     dt = dt.strftime('%Y%m%d_%H%M%S')
     filename = f"{dt}-{state}-{results['functionName']}-{results['functionRegion']}.txt"
-    s3.s3_upload_object(bytes(json.dumps(results).encode('UTF-8')), 'tcss562-x86-results', filename)
+
+    s3.s3_upload_object(bytes(json.dumps(results).encode('UTF-8')), f'tcss562-{arch}-results', filename)
     return results
