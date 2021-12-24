2eb9dc9d9b7903427b7fe8d63497c766ee125609
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2021-12-24 20:54:06 +0000

    Adding comments and retry flag to test_lambda.sh
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- test_lambda.sh
+++ test_lambda.sh
@@ -16,14 +16,25 @@ else
 	REGION=$2
 fi
 
+# function_name = $1
+# lambda_function = $2
+# filename_postfix = $3
+# directory_name = $4
+# retry = $5
 execute_lambda_function() {
 	json={"\"function_name\"":"\"$1\""}
 	# Set timeout to 10 minutes
 	mystart=`date "+%y%m%d%H%M"`
 	output=$(aws lambda invoke --cli-read-timeout 900 --invocation-type RequestResponse --function-name $2 --region $REGION --cli-binary-format raw-in-base64-out --payload "$json" /dev/stdout)
 	local ret=$?
-	if [[ $ret -ne 0 || -z "$output" ]]; then
+	if [[ $ret -ne 0 || -z "$output" || echo "$output" | grep -q 'errorType' ]]; then
 		echo "ERROR: something bad happened!"
+		if [[ -n "$5" && $5 -gt 0 ]]; then
+			retry=$((${5}-1))
+			execute_lambda_function "$1" "$2" "$3" "$4" $retry
+		else
+			exit 1
+		fi
 	else
 		echo $output | head -n 1 | head -c -2 | jq | tee $4/$mystart-$3.txt
 	fi
