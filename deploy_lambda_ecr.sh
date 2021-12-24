ab00de35fad51c801bda36ec5ced29c4f7a88865
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2021-12-24 20:40:40 +0000

    Updated deploy_lambda_ecr.sh to allow setting arch
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- deploy_lambda_ecr.sh
+++ deploy_lambda_ecr.sh
@@ -5,20 +5,18 @@ cd `dirname $0`
 # Check if awscli is installed
 ./install_awscli.sh
 
-# This is the default project lambda function name
-if [[ -z "$1" && "`uname -m`" == 'x86_64' ]]; then
-	MY_FUNCTION='tcss562_term_project'
-elif [ -z "$1" ]; then
-	MY_FUNCTION='tcss562_term_project_arm'
+# Allow users to set the arch
+if [ -z "$1" ]; then
+	ARCH=`uname -m`
 else
-	MY_FUNCTION=$1
+	ARCH=$1
 fi
 
-# Set region
+# Allow users to set the region
 if [ -z "$2" ]; then
-	MY_REGION='us-east-2'
+	REGION="us-east-2"
 else
-	MY_REGION=$2
+	REGION="$2"
 fi
 
 # Set dockerfile
@@ -28,20 +26,27 @@ else
 	DOCKERFILE=$3
 fi
 
+# This is the default project lambda function name
+if [ -z "$4" ]; then
+	FUNCTION="topic-modeling-$REGION-$ARCH"
+else
+	FUNCTION=$4
+fi
+
 # Extract Account ID
 id=`aws sts get-caller-identity | sed -n 's|.*Account.*"\([0-9]\+\)".*|\1|p'`
 
 # My container registry portal
-MY_ECR=$id.dkr.ecr.$MY_REGION.amazonaws.com
+ECR=$id.dkr.ecr.$REGION.amazonaws.com
 unset id
 
 # Create ECR repo (this step will fail if it exists but we will assume that all is well)
-aws ecr create-repository --repository-name $MY_FUNCTION --region $MY_REGION 2> /dev/null
+aws ecr create-repository --repository-name $FUNCTION --region $REGION 2> /dev/null
 
 # Build/deploy Docker image
-docker build -f $DOCKERFILE -t $MY_ECR/$MY_FUNCTION .
-aws ecr get-login-password --region $MY_REGION | docker login --username AWS --password-stdin $MY_ECR
-docker push $MY_ECR/$MY_FUNCTION:latest
+docker build -f $DOCKERFILE -t $ECR/$FUNCTION .
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR
+docker push $ECR/$FUNCTION:latest
 
 # Deploy lambda
-#aws lambda update-function-code --function-name $MY_FUNCTION --image-uri $MY_ECR/$MY_FUNCTION:latest --region $MY_REGION
+#aws lambda update-function-code --function-name $FUNCTION --image-uri $ECR/$FUNCTION:latest --region $REGION
